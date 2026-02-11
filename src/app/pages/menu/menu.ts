79fc1b70aa03cb53d9423f391ab20a85a04eecce
matheeshapr <matheeshap62@gmail.com>
2026-02-11 16:15:22 +0530

    update menu.ts

--- src/app/pages/menu/menu.ts
+++ src/app/pages/menu/menu.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { ItemService } from '../../service/items/item'; 
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule], 
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
-export class Menu {
+export class Menu implements OnInit{
+  
+  itemForm: FormGroup;
+  itemList: any[] = [];
 
+  constructor(
+    private itemService: ItemService,
+    private fb: FormBuilder
+  ){
+    this.itemForm = this.fb.group({
+      name: [''],
+      description: [''],
+      price: [''],
+      imageUrl: ['']
+
+    })
+  }
+
+   ngOnInit(): void {
+    this.loadItems();
+  }
+
+  loadItems(){
+    this.itemService.getItems().subscribe({
+      next: (data) => {
+        this.itemList = data;
+        console.log(data);
+      }
+    })
+  }
 }
